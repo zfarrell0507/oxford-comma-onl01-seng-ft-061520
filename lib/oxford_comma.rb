cab550c30df5276412140e961562a7c2bca5111a
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-25 12:34:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,11 +3,11 @@ def oxford_comma(array)
   if array.size < 3 && array.size > 1
     array.join(" and ")
   
-  if array.size > 2
+  elsif array.size > 2
     array[-1] = "and " + array[-1] 
-    
+    array.join(", ")
   
-   if array.size > 2
+   elsif
   
   array.join if array.size <= 1
   
