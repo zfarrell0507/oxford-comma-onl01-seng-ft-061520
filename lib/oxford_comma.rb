32dca434a406771c9a8167d0bb2a4973113b18fa
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-25 13:12:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,19 +3,14 @@ def oxford_comma(array)
   if array.size == 2
     array.insert(1, "and")
     array.join(" ")
-    puts array
-    array
   
   elsif array.size > 2
     array[-1] = "and " + array[-1] 
     array.join(", ")
-    puts array
-    array
   
   else
     array.join()
-    puts array
-    array
-
+    
+  end
 end
 oxford_comma(array)
