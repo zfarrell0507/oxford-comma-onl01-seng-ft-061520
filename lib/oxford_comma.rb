3e824f791c3d0cf9a9ce7a1fa338fedc3d60a046
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-25 13:09:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -18,6 +18,6 @@ def oxford_comma(array)
     array
   
   end
-  array.join(", ") if array.size > 2 end
-  array.join(" ") if array.size == 2 end
+  array.join(", ") if array.size > 2
+  array.join(" ") if array.size == 2
 oxford_comma(array)
