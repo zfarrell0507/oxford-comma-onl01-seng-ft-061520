65c95d774a334d97bd9fda679d61f5d0ad35ae82
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-25 12:05:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,5 @@
 array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychee", "pomelos"]
 def oxford_comma(array)
-
-    def oxford_comma(array)
   array.join(' ') if array.size < 3
   array[-1] = "and " + array[-1]
   array.join(', ')
