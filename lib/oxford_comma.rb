3db08e40624da2e930b1a73468f5355f5d056a38
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-25 12:33:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,8 +5,9 @@ def oxford_comma(array)
   
   if array.size > 2
     array[-1] = "and " + array[-1] 
+    
   
-  array.join(", ") if array.size > 2
+   if array.size > 2
   
   array.join if array.size <= 1
   
