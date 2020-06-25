0990c9ae054971b70f3f3aca560bc3fd76a1631b
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-25 12:02:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -23,8 +23,11 @@ def oxford_comma(array)
     puts array
   
   elsif array.size == 6
-    array.join(", ")
-    puts array
+    def oxford_comma(list)
+  return list.join(' and ') if list.size < 3
+  list[-1] = "and " + list[-1]
+  list.join(', ')
+end
   
   elsif array.size == 7
     array.join(", ")
